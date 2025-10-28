4b20599c381fca3d42ff3331d04a09d8bd1c11ee
Anurag Borkar <anuragvborkar@gmail.com>
2025-10-28 19:34:59 +0530

    User DB modified to include time of registration

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import mapped_column
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import sql
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ class User(db.Model):
     email = mapped_column(String(64), unique=True, nullable=False)
     role = mapped_column(String(64), nullable=False)
     password = mapped_column(String(128), nullable=False)
+    creation_time = mapped_column(DateTime, default=sql.func.now(), nullable=False)
 
     def __repr__(self):
         return f'<User {self.id}>'
